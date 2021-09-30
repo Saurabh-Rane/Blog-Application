a1a5ffb37a85bcd3eb7008ee52973d5aab15db9e
Saurabh Rane <ssrane@asu.edu>
2021-09-30 16:28:08 -0700

    Initialised nodejs files and created a class with functions to read data from JSON file in NodeJS

---
+++ blog-api/api/models/posts.js
@@ -0,0 +1,26 @@
+const PATH = './data.json';
+const fs = require('fs');
+
+class Post {
+    get() {
+        /** Get Posts */
+        return this.readData();
+    }
+
+    getIndividualBlog() {
+        /** Get One Blog Post */
+    }
+
+    add() {
+        /** Add new Post */
+    }
+
+    readData() {
+        let rawdata = fs.readFileSync(PATH);
+        let posts = JSON.parse(rawdata);
+        return posts;
+    }
+
+}
+
+module.exports = Post;
