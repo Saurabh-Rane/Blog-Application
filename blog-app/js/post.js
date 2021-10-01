5361cda73632c6528243fc800860722e07bbb6d9
Saurabh Rane <ssrane@asu.edu>
2021-10-01 02:25:46 -0700

    Connected Post Page with API Data

--- blog-app/js/post.js
+++ blog-app/js/post.js
@@ -11,11 +11,32 @@ window.onload = () => {
     getPost();
 }
 
+const getPostIdParam = () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get('id');
+}
+
 const getPost = () => {
+    const postId = getPostIdParam();
     // CODE GOES HERE
+    fetch(`${API_URL}${postId}`, {
+        method: 'GET'
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+        buildPost(data);
+    })
 }
 
 const buildPost = (data) => {
-    // HINT: Convert the date number to a Date string 
+    // HINT: Convert the date number to a Date string
+    const postDate = new Date(parseInt(data.added_date)).toDateString();
+    document.querySelector('header').style.backgroundImage = `url(${API_BASE_URL}${data.post_image})`;
+    document.getElementById('individual-post-title').innerText =  data.title;
+    document.getElementById('individual-post-date').innerText = `Published on ${postDate}`;
+    document.getElementById('individual-post-content').innerText = data.content;
+    
 }
 
