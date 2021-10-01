5f2e5c6bc77a750214a2f7536abda20e89af1fa9
Saurabh Rane <ssrane@asu.edu>
2021-09-30 17:16:37 -0700

    Created API Endpoint to get an individual blog post

--- blog-api/api/models/posts.js
+++ blog-api/api/models/posts.js
@@ -7,18 +7,29 @@ class Post {
         return this.readData();
     }
 
-    getIndividualBlog() {
+    getIndividualBlog(postId) {
         /** Get One Blog Post */
+        const posts = this.readData();
+        const foundPost = posts.find((post) => post.id == postId);
+        return foundPost;
     }
 
-    add() {
+    add(newPost) {
         /** Add new Post */
+        const currentPosts = this.readData();
+        currentPosts.unshift(newPost);
+        this.storeData(currentPosts);
     }
 
     readData() {
         let rawdata = fs.readFileSync(PATH);
         let posts = JSON.parse(rawdata);
         return posts;
+    }
+
+    storeData(rawData) {
+        let data = JSON.stringify(rawData);
+        fs.writeFileSync(PATH, data);
     }
 
 }
